23ab8b9782033239f46585bbe59f543678c70c07
rezaduty <reza.duty.persian@gmail.com>
2019-10-05 00:23:52 +0330

    add comment in sql injection

--- Injection/SQL/SQL_Injection/src/main/java/com/baeldung/examples/security/sql/SqlInjectionSamplesApplication.java
+++ Injection/SQL/SQL_Injection/src/main/java/com/baeldung/examples/security/sql/SqlInjectionSamplesApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SqlInjectionSamplesApplication {
 
+	/**
+	 * Saturday - 2019 05 October
+	 * @eugenp
+	 * https://www.securecoding.ir
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SqlInjectionSamplesApplication.class, args);
 	}
